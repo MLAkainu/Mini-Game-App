d93da6a379a837ff2c97134d093a23a45f897765
MLITBKU <loc.nguyenminh1605@hcmut.edu.vn>
2024-01-19 15:21:19 +0700

    add icon

--- real-time-app/screens/GameScreen.js
+++ real-time-app/screens/GameScreen.js
@@ -1,5 +1,8 @@
 import { useState, useEffect } from "react";
 import { View, StyleSheet, SafeAreaView, Alert } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+
+
 import Title from "../components/ui/Title";
 import NumberContainer from "../components/game/NumberContainer";
 import PrimaryButton from "../components/ui/PrimaryButton";
@@ -54,14 +57,21 @@ function GameScreen({userNumber, onGameOver}) {
         <Title>Opponent's Guess</Title>
         <NumberContainer>{currentGuess}</NumberContainer>
         <Card>
-            <InstructionText>Higher or lower?</InstructionText>
-            <View>
-                <PrimaryButton onPress={nextGuessHandler.bind(this, 'lower')}>-</PrimaryButton>
-                
-            </View>
-            <View>
-                <PrimaryButton onPress={nextGuessHandler.bind(this, 'greater')}>+</PrimaryButton>
-                
+            <InstructionText style={styles.instructionText}>Higher or lower?</InstructionText>
+            <View style={styles.buttonsContainer}>
+                <View style={styles.buttonContainer}>
+                    <PrimaryButton onPress={nextGuessHandler.bind(this, 'lower')}>
+                        <Ionicons name="md-remove" size={24} color="white" />
+                    </PrimaryButton>
+                    
+                </View>
+                <View style={styles.buttonContainer}>
+                    <PrimaryButton onPress={nextGuessHandler.bind(this, 'greater')}>
+                    <Ionicons name="md-add" size={24} color="white" />
+                    </PrimaryButton>
+                    
+                </View>
+
             </View>
             
         </Card>
@@ -81,6 +91,17 @@ const styles = StyleSheet.create({
         flex: 1,
         padding: 40,
 
+    },
+    instructionText: {
+        marginTop: 12,
+        marginBottom: 12,
+    },
+    buttonsContainer: {
+        flexDirection: 'row',
+        
+    },
+    buttonContainer: {
+        flex: 1,
     },
     
 
